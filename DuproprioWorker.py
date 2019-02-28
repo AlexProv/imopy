d4cd45527d48aeb1dfc99e873697582a1b4f7282
Alex Provencher <alexprovencher@Alexs-MacBook-Pro.local>
2019-02-28 18:48:04 -0500

    oups

--- DuproprioWorker.py
+++ DuproprioWorker.py
@@ -1,44 +1,20 @@
-from google.cloud import firestore
+
+import logging
 import DuproprioScraper as DS
-import json
-from  datetime import datetime
 import argparse
 
 
-def gather_urls(dp, db):
-    dp.hit_search()
-    dp.crawler()
-    db.collection('duproprio-urls').document(str(datetime.now())).set({'urls': json.dumps(dp.search_results)})
-
-def gather_all_urls():
-    dp_worker = DS.DuproprioScrapper(cities=DS.cities)
-    gather_urls(dp_worker, firestore.Client())
-
-def crawl_urls():
-    db = firestore.Client()
-    urls_gen = db.collection('duproprio-urls').get()
-    urls = {}
-    for url in urls_gen:
-        urls[datetime.strptime(url.id, '%Y-%m-%d %H:%M:%S.%f')] = url.to_dict()
-
-    latest = sorted(urls, reverse=True)[0]
-    latest_urls = urls[latest]['urls']
-
-    urls = list(map(lambda x: x.replace('"', '').replace(' ', ''), latest_urls[1:-2].split(',')))
-
-    dp_worker = DS.DuproprioScrapper(start_url=urls.pop())
-    dp_worker.page_crawler(urls)
-
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--run', help='mode: url-scan, url-crawl')
+parser.add_argument('--run', help='mode: url-scan, url-crawl, full-scan')
 args = parser.parse_args()
+dp_worker = DS.DuproprioScrapper(cities=DS.cities)
 if args.run == 'url-scan':
-    gather_all_urls()
+    dp_worker.scan_new_urls()
 elif args.run == 'url-crawl':
-    crawl_urls()
+    dp_worker.crawl_new_urls()
 elif args.run == 'full-scan':
-    print('gatering urls')
-    gather_all_urls()
-    print('\n CRAWLING START \n \n \n')
-    crawl_urls()
+    logging.info('gathering urls')
+    dp_worker.scan_new_urls()
+    logging.info('crawling urls')
+    dp_worker.crawl_new_urls()
